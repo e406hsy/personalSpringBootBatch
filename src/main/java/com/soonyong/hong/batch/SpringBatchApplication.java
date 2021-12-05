8fdd9695e5bd09cfafb1971dcf49e3e14ae9b99a
e406hsy <e406hsy@gmail.com>
2021-12-05 10:07:26 +0900

    feat : add basic spring boot batch setting

---
+++ src/main/java/com/soonyong/hong/batch/SpringBatchApplication.java
@@ -0,0 +1,16 @@
+package com.soonyong.hong.batch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class SpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchApplication.class, args);
+	}
+
+}
